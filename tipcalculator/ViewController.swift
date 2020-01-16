5a300207461dccfcab1cf6a3e02bfd38d4a17f1e
Monique Barbour <moniquebarbour15@gmail.com>
2020-01-16 16:54:45 -0500

    Initial Commit

---
+++ tipcalculator/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  tipcalculator
+//
+//  Created by Monique Barbour on 1/16/20.
+//  Copyright © 2020 codepath. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
